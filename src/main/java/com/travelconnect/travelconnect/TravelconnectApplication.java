0ad3e715c8a9543846793703e57dcc402ae66084
Aryan Mishra <maryan266@gmail.com>
2024-10-18 23:13:46 +0530

    Configured DB settings in application.properties

---
+++ src/main/java/com/travelconnect/travelconnect/TravelconnectApplication.java
@@ -0,0 +1,13 @@
+package com.travelconnect.travelconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelconnect/travelconnect/TravelconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelconnect.travelconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
